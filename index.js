5fbb57d90b1530600fc1781e32d29e49b5f9a596
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-04 21:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function takeANumber(katzDeliLine,newPerson) {
   katzDeliLine.push(newPerson)
   return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing
