7fa9a5ccc72d32e0e2542644a4fe9cdc7d50c627
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-04 21:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(num, name) {
     line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${num.length} in line.`
 }
